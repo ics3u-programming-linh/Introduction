864dac58c7444a78426a37879db722387da16262
Linh <46288798+s272936@users.noreply.github.com>
2019-09-06 13:02:17 -0400

    ProgramMenu

---
+++ ProgramMenuLinh/ProgramMenuLinh/ProgramMenuForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace ProgramMenuLinh
+{
+    partial class ProgramMenufrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProgramMenufrm));
+            this.lblTvShow = new System.Windows.Forms.Label();
+            this.lblActor = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.mnuMenu = new System.Windows.Forms.MenuStrip();
+            this.mniProgram = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.mnuMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTvShow
+            // 
+            this.lblTvShow.AutoSize = true;
+            this.lblTvShow.BackColor = System.Drawing.Color.Transparent;
+            this.lblTvShow.Font = new System.Drawing.Font("MV Boli", 35F);
+            this.lblTvShow.Location = new System.Drawing.Point(395, 140);
+            this.lblTvShow.Name = "lblTvShow";
+            this.lblTvShow.Size = new System.Drawing.Size(369, 62);
+            this.lblTvShow.TabIndex = 0;
+            this.lblTvShow.Text = "The Good Place";
+            this.lblTvShow.Click += new System.EventHandler(this.LblTvShow_Click);
+            // 
+            // lblActor
+            // 
+            this.lblActor.AutoSize = true;
+            this.lblActor.BackColor = System.Drawing.Color.Transparent;
+            this.lblActor.Font = new System.Drawing.Font("ISOCTEUR", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblActor.Location = new System.Drawing.Point(428, 202);
+            this.lblActor.Name = "lblActor";
+            this.lblActor.Size = new System.Drawing.Size(320, 44);
+            this.lblActor.TabIndex = 1;
+            this.lblActor.Text = "Kristen Bell";
+            this.lblActor.Click += new System.EventHandler(this.LblActor_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 70);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(377, 285);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // mnuMenu
+            // 
+            this.mnuMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniProgram});
+            this.mnuMenu.Location = new System.Drawing.Point(0, 0);
+            this.mnuMenu.Name = "mnuMenu";
+            this.mnuMenu.Size = new System.Drawing.Size(800, 24);
+            this.mnuMenu.TabIndex = 3;
+            this.mnuMenu.Text = "Menu by Linh Ho";
+            // 
+            // mniProgram
+            // 
+            this.mniProgram.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniProgram.Name = "mniProgram";
+            this.mniProgram.Size = new System.Drawing.Size(65, 20);
+            this.mniProgram.Text = "Program";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(180, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.MniExit_Click);
+            // 
+            // ProgramMenufrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTvShow);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblActor);
+            this.Controls.Add(this.mnuMenu);
+            this.MainMenuStrip = this.mnuMenu;
+            this.Name = "ProgramMenufrm";
+            this.Text = "Program Menu by Linh Ho";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.mnuMenu.ResumeLayout(false);
+            this.mnuMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTvShow;
+        private System.Windows.Forms.Label lblActor;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip mnuMenu;
+        private System.Windows.Forms.ToolStripMenuItem mniProgram;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+    }
+}
+
